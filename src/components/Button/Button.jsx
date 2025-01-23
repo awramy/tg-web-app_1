592a5f17ac28a4161dc323e10a1b66f200bc1318
awramy <avramchech@gmail.com>
2025-01-23 17:43:15 +0300

    init

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import './Button.css'
 const Button = (props) => {
   return (
     // eslint-disable-next-line react/prop-types
-    <button {...props} className={'button' + props.className} />
+    <button {...props} className={'button ' + props.className} />
   );
 };
 
